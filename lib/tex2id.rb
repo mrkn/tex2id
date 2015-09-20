cc7e7750a413e692b2edb4ea03d0c9a0974adf61
Kenta Murata <mrkn@cookpad.com>
2015-09-20 11:13:44 +0900

    Add empty Tex2id::Converter

--- lib/tex2id.rb
+++ lib/tex2id.rb
@@ -2,5 +2,9 @@ require "tex2id/version"
 
 module Tex2id
   def self.convert(source_filename)
+    source = IO.read(source_filename, mode: 'r:UTF-8')
+    Converter.new(source).convert
   end
 end
+
+require "tex2id/converter"
---
+++ lib/tex2id/converter.rb
@@ -0,0 +1,8 @@
+class Tex2id::Converter
+  def initialize(source)
+    @source = source
+  end
+
+  def convert
+  end
+end
