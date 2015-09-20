b269ed6514d909a3f2fea787fe1d58a0841e03f3
Kenta Murata <mrkn@cookpad.com>
2015-09-20 13:13:59 +0900

    Implement Tex2id::Converter.convert

--- lib/tex2id/converter.rb
+++ lib/tex2id/converter.rb
@@ -4,5 +4,116 @@ class Tex2id::Converter
   end
 
   def convert
+    pass1 = convert_display_math(@source)
+    pass2 = convert_inline_math(pass1)
+    pass2
+  end
+
+  def convert_display_math(source)
+    source.gsub(/\$\$(.*?)\$\$/m) {
+      [ "<pstyle:半行アキ>",
+        "<pstyle:数式>" + convert_tex_source($1.strip),
+      ].join("\n")
+    }.gsub(/<ParaStyle:本文>(<pstyle:半行アキ>)/) { $1 }
+  end
+
+  def convert_inline_math(source)
+    source.gsub(/\$(.*?)\$/m) do
+      convert_tex_source($1.strip)
+    end
+  end
+
+  TOKEN_PATTERN = %r[
+    # [0] white spaces
+    (\s+)
+  |
+    # [1] macros
+    (
+      '
+    |
+      \\(?:
+        c?dots
+      | times
+      | q?quad
+      | sigma
+      | Delta
+      | varepsilon
+      | ell
+      | max
+      | min
+      )
+    )
+  |
+    # [2] roman mathop
+    \\mathop\{\\mathrm\{(\w+)\}\}
+  |
+    # [3] normal chars
+    ([a-zA-Z0-9\(\)=+]+)
+  |
+    # subscript
+    _(?:
+      ([\w\d])  # [4] single character
+     |
+      \{(
+        (?:\\\}|[^\}])+ # [5] multiple characters
+      )\}
+    )
+    (?:
+      # optional superscript
+      \^(?:
+        ([\w\d])  # [6] single character
+      |
+        \{(
+          (?:\\\}|[^\}])+ # [7] multiple characters
+        )\}
+      )
+    )?
+  |
+    # [8] superscript
+    \^([\w\d])
+  |
+    # [9] superscript
+    \^\{((?:\\\}|[^\}])+)\}
+  |
+    (.) [10] other character
+  ]x
+
+  MACROS = {
+    "'"      => '<cstyle:数式><2032><cstyle:>',
+    '\dots'  => '<cstyle:数式>...<cstyle:>',
+    '\cdots' => '<cstyle:数式><22EF><cstyle:>',
+    '\times' => '<cstyle:数式>×<cstyle:>',
+    '\quad'  => '<cstyle:数式>　<cstyle:>',
+    '\qquad' => '<cstyle:数式>　　<cstyle:>',
+    '\sigma' => '<cstyle:数式イタリック><clig:0><cotfcalt:0><F0BE><clig:><cotfcalt:><cstyle:>',
+    '\Delta' => '<cstyle:数式イタリック><clig:0><cotfcalt:0><F0A2><clig:><cotfcalt:><cstyle:>',
+    '\varepsilon' => '<cstyle:数式イタリック><clig:0><cotfcalt:0><F022><clig:><cotfcalt:><cstyle:>',
+    '\ell' => '<cstyle:数式イタリック><clig:0><cotfcalt:0><F060><clig:><cotfcalt:><cstyle:>',
+    '\max' => '<cstyle:数式ローマン>max<cstyle:>',
+    '\min' => '<cstyle:数式ローマン>min<cstyle:>',
+  }.freeze
+
+  def convert_tex_source(source)
+    source.scan(TOKEN_PATTERN).map { |token|
+      if (tok = token[0])
+        tok
+      elsif (tok = token[1])
+        MACROS[tok]
+      elsif (tok = token[2])
+        "<cstyle:数式ローマン>" + tok + "<cstyle:>"
+      elsif (tok = token[3])
+        "<cstyle:数式>" + tok + "<cstyle:>"
+      elsif (tok = token[4] || token[5])
+        if (tok2 = token[6] || token[7])
+          "<cstyle:数式下付き><cr:1><crstr:#{tok2}>" + tok + "<cr:><crstr:><cstyle:>"
+        else
+          "<cstyle:数式下付き>" + tok + "<cstyle:>"
+        end
+      elsif (tok = token[8] || token[9])
+        "<cstyle:数式上付き>" + tok + "<cstyle:>"
+      else
+        token[10]
+      end
+    }.join('')
   end
 end
---
+++ spec/tex2id/converter_spec.rb
@@ -0,0 +1,123 @@
+require 'spec_helper'
+
+RSpec.describe Tex2id::Converter do
+  describe '#convert' do
+    let(:source) do |example|
+      example.full_description[/source='((?:\\'|[^'])*)'/, 1]
+    end
+
+    subject(:converter) do
+      Tex2id::Converter.new(source)
+    end
+
+    def self.it_converts_source_to(expected_output)
+      it %Q[converts source to '#{expected_output}'] do
+        expect(subject.convert).to eq(expected_output)
+      end
+    end
+
+    context "for source='<SJIS-MAC>\n'" do
+      it_converts_source_to("<SJIS-MAC>\n")
+    end
+
+    context "for source='<ParaStyle:中見出し>基本\n'" do
+      it_converts_source_to("<ParaStyle:中見出し>基本\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y_1$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:><cstyle:数式下付き>1<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y_{12}$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:><cstyle:数式下付き>12<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y^1$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:><cstyle:数式上付き>1<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y^{12}$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:><cstyle:数式上付き>12<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y_{12}^{3}$\n'" do
+      # TODO: need to confirm to @inao
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:><cstyle:数式下付き><cr:1><crstr:3>12<cr:><crstr:><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$y_{12}^{(3)}$\n'" do
+      # TODO: need to confirm to @inao
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>y<cstyle:><cstyle:数式下付き><cr:1><crstr:(3)>12<cr:><crstr:><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\dots$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>...<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\cdots$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式><22EF><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\times$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>×<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\quad$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式>　<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\'$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式><2032><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\sigma$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式イタリック><clig:0><cotfcalt:0><F0BE><clig:><cotfcalt:><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\Delta$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式イタリック><clig:0><cotfcalt:0><F0A2><clig:><cotfcalt:><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\varepsilon$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式イタリック><clig:0><cotfcalt:0><F022><clig:><cotfcalt:><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\ell$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式イタリック><clig:0><cotfcalt:0><F060><clig:><cotfcalt:><cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\max$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式ローマン>max<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\min$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式ローマン>min<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>$\\mathop{\\mathrm{AND}}$\n'" do
+      it_converts_source_to("<ParaStyle:本文><cstyle:数式ローマン>AND<cstyle:>\n")
+    end
+
+    context "for source='<ParaStyle:本文>関数は $f(u) = z_{j}^{(2)} = f(w_{11} x^1 + w_{21}^{(2)} x_2 + w)$ になります。\n'" do
+      it_converts_source_to("<ParaStyle:本文>関数は <cstyle:数式>f(u)<cstyle:> <cstyle:数式>=<cstyle:> <cstyle:数式>z<cstyle:><cstyle:数式下付き><cr:1><crstr:(2)>j<cr:><crstr:><cstyle:> <cstyle:数式>=<cstyle:> <cstyle:数式>f(w<cstyle:><cstyle:数式下付き>11<cstyle:> <cstyle:数式>x<cstyle:><cstyle:数式上付き>1<cstyle:> <cstyle:数式>+<cstyle:> <cstyle:数式>w<cstyle:><cstyle:数式下付き><cr:1><crstr:(2)>21<cr:><crstr:><cstyle:> <cstyle:数式>x<cstyle:><cstyle:数式下付き>2<cstyle:> <cstyle:数式>+<cstyle:> <cstyle:数式>w)<cstyle:> になります。\n")
+    end
+
+    context "for source='<ParaStyle:本文>$$y$$\n'" do
+      it_converts_source_to(<<-END_RESULT)
+<pstyle:半行アキ>
+<pstyle:数式><cstyle:数式>y<cstyle:>
+      END_RESULT
+    end
+
+    context "for source='<ParaStyle:本文>$$ y = f(x) $$\n'" do
+      it_converts_source_to(<<-END_RESULT)
+<pstyle:半行アキ>
+<pstyle:数式><cstyle:数式>y<cstyle:> <cstyle:数式>=<cstyle:> <cstyle:数式>f(x)<cstyle:>
+      END_RESULT
+    end
+  end
+end
